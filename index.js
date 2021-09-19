9a2c2345043f7ed126c0ca194df4c29e111a140c
Aaron Dalzell <aarondalzell16@gmail.com>
2021-09-19 21:11:47 +0100

    Included contact page link, and photo/css folders

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require("express");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 
+
 const app = express();
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 // view directory for product and form views
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "photos")));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", function(req, res){
    res.render("index");
+})
+
+app.get("/contact", function(req, res){
+   res.render("contact");
 })
 
 app.listen(3000, function(){
